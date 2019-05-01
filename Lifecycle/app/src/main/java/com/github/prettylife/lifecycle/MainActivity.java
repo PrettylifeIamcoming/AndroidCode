98e05f6a1314a4ebb9385e5cbab71c687ae256ba
Mike <729186076@qq.com>
2019-05-01 17:38:31 +0800

    Add ctivity Lifecycle.

--- Lifecycle/app/src/main/java/com/github/prettylife/lifecycle/MainActivity.java
+++ Lifecycle/app/src/main/java/com/github/prettylife/lifecycle/MainActivity.java
@@ -1,26 +1,57 @@
 package com.github.prettylife.lifecycle;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = findViewById(R.id.main_button);
+        Button button = findViewById(R.id.main_button1);
         button.setText("SecondActivity");
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, SecondActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        final AlertDialog dialog = new AlertDialog.Builder(this)
+                .setTitle("我是对话框")//设置对话框的标题
+                .setMessage("我是对话框的内容")//设置对话框的内容
+                //设置对话框的按钮
+                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, "点击了取消按钮", Toast.LENGTH_SHORT).show();
+                        dialog.dismiss();
+                    }
+                })
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, "点击了确定的按钮", Toast.LENGTH_SHORT).show();
+                        dialog.dismiss();
+                    }
+                }).create();
+
+        Button button1 = findViewById(R.id.main_button2);
+        button1.setText("OpenDialog");
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.show();
             }
         });
 
